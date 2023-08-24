2789c0a24c75015eca180a8d507ed32c1efe7170
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2023-08-24 13:14:13 -0500

    worksheet

---
+++ Worksheet 1.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print("Hello, Moon!")
+
+print("Hii",10,12.25)
+
+var programmingLanguage = "Swift"
+print("My favorite programming language is \(programmingLanguage)")
+
+var age = 23
+print("you are \(age) years old and in another \(age) years, you will be \(age * 2)")
+
+print("""
+Hello
+World!
+""")
+
+print("Hello All, \rWelcome to Swift programming")
+
+let welcomeMessage : String = "Hello"
+print(welcomeMessage , "All")
+
+print("Welcome to Swift programming")
+print("Fall 2021")
+print("**********")
+print("Welcome to Swift programming", terminator : "-")
+print("Fall 2021")
+
+print("The list of numbers are ")
+print(1,2,3,4,5,6)
+print("The new pattern is")
+print(1,2,3,4,5,6, separator: "-")
